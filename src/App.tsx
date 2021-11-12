d0e09d4342a61bf1fa6c28d003146bb3fbb7b423
bricio123 <fabricio1234c@gmail.com>
2021-11-11 21:51:29 -0300

    adding more components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import styled from 'styled-components'
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
 export function App() {
   return (
-    <div className="App">
-      <h1> Hello world</h1>
-    </div>
+    <>
+      <Header/>
+      <GlobalStyle/>
+    </>
   );
 }
 
